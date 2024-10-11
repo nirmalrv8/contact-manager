aeb44f1df0a8dbdf07ab4f0fec368a90e7d96fc8
nirmalrv8 <nirmal.seneviratne@gmail.com>
2024-10-12 11:47:25 +1300

    Changed directory of project.

---
+++ backend/Migrations/20241010233513_AddTypeToContact.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace contact_manager_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTypeToContact : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "contactType",
+                table: "Contacts",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "contactType",
+                table: "Contacts");
+        }
+    }
+}
